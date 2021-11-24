e5cbb906b92900f3d743e6275542234759d3a734
shakeel26 <xhakil26@gmail.com>
2021-11-24 10:15:36 +0100

    Remove comments

--- src/components/Documentation/Documentation.js
+++ src/components/Documentation/Documentation.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import Verification from "./Verification";
 import Legalization from "./Legalization";
 import Admission from '../AdmissionResidence/Admission';
 import Divider from '@material-ui/core/Divider';
@@ -12,14 +11,11 @@ const Documentation = () => {
         <Grid container spacing={0}>
 
             <Grid item xs={12} sm={12} md={6} lg={6}>
-                {/* <Verification /> */}
                 <Legalization />
                 <Divider />
             </Grid>
 
             <Grid item xs={12} sm={12} md={6} lg={6}>
-                {/* <Legalization /> */}
-                {/* <Divider /> */}
                 <Admission />
                 <Divider />
                 <Accommodation />
